67e4711bc29dabed04e567ec1d7ab7c2120c7412
Soonho Kim <sunoj72@naver.com>
2016-08-03 13:28:13 +0000

    initialize the project

---
+++ local-server/routes.js
@@ -0,0 +1,69 @@
+(function() {
+  'use strict';
+  var express = require('express');
+  var router = express.Router();
+  var fs = require('fs');
+  var path = require('path');
+
+  /* GET home page. */
+  // router.get('/', function(req, res) {
+  //   res.render('index');
+  // });
+
+
+
+  /* Serve the Tree */
+  router.get('../config/data/tree', function(req, res) {
+    var _p;
+    if (req.query.id == 1) {
+      _p = path.resolve(__dirname, '..', 'node_modules');
+      processReq(_p, res);
+
+    } else {
+      if (req.query.id) {
+        _p = req.query.id;
+        processReq(_p, res);
+      } else {
+        res.json(['No valid data found']);
+      }
+    }
+  });
+
+  /* Serve a Resource */
+  router.get('/api/resource', function(req, res) {
+    res.send(fs.readFileSync(req.query.resource, 'UTF-8'));
+  });
+
+  function processReq(_p, res) {
+    var resp = [];
+    fs.readdir(_p, function(err, list) {
+      for (var i = list.length - 1; i >= 0; i--) {
+        resp.push(processNode(_p, list[i]));
+      }
+      res.json(resp);
+    });
+  }
+
+  function processNode(_p, f) {
+    var s = fs.statSync(path.join(_p, f));
+    return {
+      "id": path.join(_p, f),
+      "text": f,
+      "type" : s.isDirectory() ? 'folder' : 'workflow',
+      // "icon" : s.isDirectory() ? 'tree-icon tree-folder' : 'tree-icon tree-file',
+      // "state": {
+      //   "opened": false,
+      //   "disabled": false,
+      //   "selected": false
+      // },
+      // "li_attr": {
+      //   "base": path.join(_p, f),
+      //   "isLeaf": !s.isDirectory()
+      // },
+      "children": s.isDirectory()
+    };
+  }
+
+  module.exports = router;
+
+}());
